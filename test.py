a0e03215d8635eee993b908e303434004109d978
MikeSphx <kim.michae@husky.neu.edu>
2019-03-20 14:45:19 -0400

    Setup 1 episode run env for lunar lander

---
+++ test.py
@@ -0,0 +1,17 @@
+import gym
+env = gym.make('LunarLander-v2')
+observation = env.reset()
+
+for episode_index in range(1):
+    observation = env.reset()
+
+    for t in range(1000):
+        env.render()
+        #print(observation)
+
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
